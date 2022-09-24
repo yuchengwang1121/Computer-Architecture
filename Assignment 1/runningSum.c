cf2acc0aba2671ae98ccdabbd91842a2d053e630
yuchengwang1121 <73687292+yuchengwang1121@users.noreply.github.com>
2022-09-24 12:25:31 +0800

    Update runningSum.c

--- Assignment 1/runningSum.c
+++ Assignment 1/runningSum.c
@@ -1,1 +1,14 @@
+#include <stdlib.h>
 
+int* runningSum(int* nums, int numsSize, int* returnSize){
+    int* result = (int*)malloc(sizeof(int)*numsSize);
+    
+    
+    for(int i=0; i<numsSize; i++){
+        if(i==0) result[i] = nums[i];
+        else result[i] = result[i-1] + nums[i];
+    }
+    
+    *returnSize = numsSize;
+    return result;
+}
