7028bc266dc74328bdea84ec67497cd2342ddf84
yuchengwang1121 <73687292+yuchengwang1121@users.noreply.github.com>
2022-10-07 01:44:56 +0800

    Modify the code at 10/6

--- Assignment 1/runningSum.s
+++ Assignment 1/runningSum.s
@@ -1,63 +1,62 @@
 .data
-nums: .word 1,2,3,4,5
-numsize: .word 5
-res: .word 0,0,0,0,0
+nums1:     .word 1,2,3,4,5
+nums2:     .word 1,3,5,7,9,11
+nums3:     .word 0,2,4,6,8,10,12
+res1:      .word 0,0,0,0,0,
+res2:      .word 0,0,0,0,0,0
+res3:      .word 0,0,0,0,0,0,0
+space:     .string " "
+nl:        .string "\n"
 
 .text
 main:
-    jal ra, runningSum
-    j print
+    # t2: nums head, t3: result head, t4: numsSize
+    la      t2, nums1      # load arr base to t2
+    la      t3, res1       # load res base to t3
+    li      t4, 5          # load the numsize to t4
+    jal     ra, runningSum
+    la      t2, nums2      # load arr base to t2
+    la      t3, res2       # load res base to t3
+    li 	    t4, 6          # load the numsize to t4
+    jal     ra, runningSum
+    la      t2, nums3      # load arr base to t2
+    la      t3, res3       # load res base to t3
+    li 	    t4, 7          # load the numsize to t4
+    jal     ra, runningSum
+    j       exit
 	
 runningSum:
-    # t2: nums head, t3: result head, t4: numsSize, t5: i, t6: j
-    # initialize
-    la      t2, nums       # load arr base to t2
-    la      t3, res        # load arr base to t2
-    la 	    t4, numsize
-    lw      t4, 0(t4)      # load the numsize to t3
-    li	    t5, 0          # load i with 0
-    li	    t6, 0          # load j with 0
-    j       end.L1         # jump to L1
+    # t5: i, t6: j         # initialize
+    li	    t5, 1          # load i with 1
+    lw      a3, 0(t2)      # load nums[0] to a3
+    sw      a3, 0(t3)      # save num[0] to result[0]
 end.L1:
-    bge     t5, t4, end.L6 # if i >= numsSize then branch to L6
-    j       end.L2         # jump to L2
-end.L2:
-    bnez    t5, end.L4     # if i != 0 then branch to L4
-    j       end.L3         # jump to L3
-end.L3:
-    lw      a0, 0(t2)      # load nums[0] to a0
-    sw      a0, 0(t3)      # save num[0] to result[0]
-    j       end.L5         # jump to L5
-end.L4:
-    addi    a0, t3, 0      # load result's address to a0
-    addi    a1, t5, 0      # load i to a1
-    slli    a3, a1, 2      # i*4 (cause of byte size)
-    add     a1, a0, a3     # add result's address with i*4 
-                           # to get result[i]'s address
-    lw      a0, -4(a1)     # load result[i-1] to a0
-    addi    a2, t2, 0      # load nums's address
-    add     a2, a2, a3     # add nums's address with i*4 
-                           # to get nums[i]'s address
-    lw      a2, 0(a2)      # load nums[i] to a2
-    add     a0, a0, a2     # add result[i-1] + nums[i]
-    sw      a0, 0(a1)      # save to result[i]
-    j       end.L5         # jump to L5
-end.L5:
+    slli    a0, t5, 2      # i*4 (cause of byte size)
+    add     a1, t3, a0     # add result's address with i*4 to get result[i]'s address
+    lw      a3, -4(a1)     # load result[i-1] to a3
+    add     a2, t2, a0     # add nums's address with i*4 to get nums[i]'s address
+    lw      a2, 0(a2)      # load nums[i] to a5
+    add     a3, a3, a2     # add result[i-1] + nums[i]
+    sw      a3, 0(a1)      # save to result[i]
     addi    t5, t5, 1      # i = i + 1 
-    j       end.L1         # jump to L1
-end.L6:
-    jr      ra             # retuen to main
-        
+    blt     t5, t4, end.L1 # if i >= numsSize then branch to L3
+    li      t5, 0
+
 print:
-    slli    a1, t6, 2
-    add	    a2, t3, a1
-    lw 	    a0, 0(a2)      # load result out
+    slli    a1, t5, 2
+    add	    a1, t3, a1
+    lw 	    a0, 0(a1)      # load result out
     li      a7, 1          # pint a0
     ecall
-    addi    t6, t6, 1      # j++
-    blt     t6, t5, print  # loop
-    j	    exit
-    
+    la      a0, space      #load space
+    li      a7, 4          #print string
+    ecall
+    addi    t5, t5, 1      # j++
+    blt     t5, t4, print  # loop
+    la      a0, nl         #load next line
+    li      a7, 4          #print string
+    ecall
+    jr    ra
 exit:
     li a7, 10		    # exit
     ecall
