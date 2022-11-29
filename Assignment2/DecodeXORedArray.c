3e8fbc9f8e33a48c42c348613cfe8f5b68ad3a3c
yuchengwang1121 <brian0978599918@gmail.com>
2022-11-29 17:37:20 +0800

    Update DecodeXORedArray.c

--- Assignment2/DecodeXORedArray.c
+++ Assignment2/DecodeXORedArray.c
@@ -1,10 +1,18 @@
-#include<stdio.h>
-int* decode(int* encoded, int encodedSize, int first, int* returnSize){
+#include <stdlib.h>
+#include <stdio.h>
+int* decode(int* encoded, int encodedSize, int first){
     int* result = (int*)malloc(sizeof(int)*(encodedSize+1));
     result[0]=first;
     for(int i=0;i<encodedSize;i++){
         result[i+1]=result[i] ^ encoded[i];
     }
-    *returnSize = encodedSize+1;
     return result;
+}
+int main(){
+    int nums1[4] = {6,2,7,3};
+    int* result1 = decode(nums1, 4, 4);
+    printf("THe result is : ");
+    for(int j=0; j<5; j++){
+        printf("%d ", result1[j]);
+    }
 }
